59033e89ed1a5af1f56e2f90a0d82f18b8bf6627
Nathaniel Butterworth <natbutter@gmail.com>
2019-03-17 17:53:49 +1100

    added intro notebook

---
+++ Week6/helloworld_advanced.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+A Python program which greets the Earth!
+
+natbutter
+AGCC 2018 Adelaide
+
+usage: python helloworld_advanced.py
+
+'''
+
+def main():
+    print("Hello World! This is basically the same result, but a different way to get there.")
+
+if __name__ == '__main__':
+    main()
